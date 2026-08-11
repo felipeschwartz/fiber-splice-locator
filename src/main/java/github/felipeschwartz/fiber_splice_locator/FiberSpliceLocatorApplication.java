f948ab0956c6991fa1ee536022adb93663df8f46
Felipe Schwartz <felipeschwartz@gmail.com>
2026-08-11 15:59:35 -0300

    first commit

---
+++ src/main/java/github/felipeschwartz/fiber_splice_locator/FiberSpliceLocatorApplication.java
@@ -0,0 +1,13 @@
+package github.felipeschwartz.fiber_splice_locator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiberSpliceLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiberSpliceLocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/felipeschwartz/fiber_splice_locator/FiberSpliceLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package github.felipeschwartz.fiber_splice_locator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiberSpliceLocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
