a1ef9f8d81ceda030fc5957b4905e2a3ceec610f
Felipe Schwartz <felipeschwartz@gmail.com>
2026-08-27 13:58:05 -0300

    Fixed some bugs of mapping.

--- src/main/java/github/felipeschwartz/fiber_splice_locator/mapper/ServiceOrderMapper.java
+++ src/main/java/github/felipeschwartz/fiber_splice_locator/mapper/ServiceOrderMapper.java
@@ -2,6 +2,7 @@ package github.felipeschwartz.fiber_splice_locator.mapper;
 
 import github.felipeschwartz.fiber_splice_locator.model.dto.ServiceOrderDTO;
 import github.felipeschwartz.fiber_splice_locator.model.entities.ServiceOrder;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
@@ -19,7 +20,12 @@ public interface ServiceOrderMapper {
     @Mapping(target = "createdAt", ignore = true)
     ServiceOrder toEntity(ServiceOrderDTO dto);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = org.mapstruct.NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(target = "serviceOrderId", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "ceo", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "serviceOrderPhotos", ignore = true)
+    @Mapping(target = "serviceOrderStatusDescriptions", ignore = true)
     void updateEntityFromDTO(ServiceOrderDTO dto, @MappingTarget ServiceOrder entity);
 }
